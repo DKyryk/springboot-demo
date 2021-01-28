b0e69173085290ce023fafefc5ad78e8e26c8821
dkyryk <kyryk.dmytro@gmail.com>
2021-01-28 23:11:05 +0200

    remove repository

--- clock-service/src/main/java/com/ercarts/clock/repository/ClockRepository.java
+++ clock-service/src/main/java/com/ercarts/clock/repository/ClockRepository.java
@@ -2,11 +2,9 @@ package com.ercarts.clock.repository;
 
 import com.ercarts.clock.dao.ClockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 /**
  * @author dkyryk
  */
-@Repository
 public interface ClockRepository extends JpaRepository<ClockEntity, Long> {
 }

0fda7729de97b54945801fde068e6a2e81280cec
dkyryk <kyryk.dmytro@gmail.com>
2021-01-29 21:30:19 +0200

    security config

--- web/src/main/java/com/ercarts/springboot/demo/web/config/SecurityConfig.java
+++ web/src/main/java/com/ercarts/springboot/demo/web/config/SecurityConfig.java
@@ -2,11 +2,16 @@ package com.ercarts.springboot.demo.web.config;
 
 import com.ercarts.springboot.demo.web.service.AuthUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
@@ -14,10 +19,28 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private AuthUserDetailsService userDetailsService;
+
+    @Bean
+    public GrantedAuthoritiesMapper authoritiesMapper() {
+        SimpleAuthorityMapper mapper = new SimpleAuthorityMapper();
+        mapper.setConvertToUpperCase(true);
+        mapper.setDefaultAuthority("USER");
+        return mapper;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(10));
+        provider.setAuthoritiesMapper(authoritiesMapper());
+        return provider;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -32,9 +55,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout().permitAll();
     }
 
-    @Autowired
-    public void ConfigureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(new BCryptPasswordEncoder(10));
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
     }
 }
--- web/src/main/java/com/ercarts/springboot/demo/web/controller/RoomController.java
+++ web/src/main/java/com/ercarts/springboot/demo/web/controller/RoomController.java
@@ -1,9 +1,15 @@
 package com.ercarts.springboot.demo.web.controller;
 
+import java.util.Collections;
+import java.util.List;
+
+import com.ercarts.springboot.demo.web.model.Room;
 import com.ercarts.springboot.demo.web.service.RoomService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -22,8 +28,18 @@ public class RoomController {
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_USER')")
     public String getAllRooms(Model model) {
         model.addAttribute("rooms", roomService.getRooms());
+        return ROOMS_VIEW;
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String get(@PathVariable long id, Model model) {
+        List<Room> rooms = roomService.getById(id).map(Collections::singletonList)
+                .orElse(Collections.emptyList());
+        model.addAttribute("rooms", rooms);
         return ROOMS_VIEW;
     }
 }

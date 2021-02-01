d24318442554e9bc3abfcadfc07c08aa9442fa01
dkyryk <kyryk.dmytro@gmail.com>
2021-02-01 18:55:19 +0200

    add authorization server skeleton

---
+++ clock-authorization-server/src/main/java/com/ercarts/clock/authorization/ClockAuthorizationServer.java
@@ -0,0 +1,15 @@
+package com.ercarts.clock.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dkyryk
+ */
+@SpringBootApplication
+public class ClockAuthorizationServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClockAuthorizationServer.class, args);
+    }
+}
